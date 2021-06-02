3280d806fe21df901a6e467204353acb0b57150a
rabobahago <rabobahago@gmail.com>
2021-06-02 17:51:39 +0100

    directoty and menu item components added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import HomePage from "./home.component.jsx";
+import HomePage from "./pages/homepage/homepage.jsx";
 
 function App() {
   return (
---
+++ src/components/menu-item/menu-item.jsx
@@ -0,0 +1,15 @@
+import "./menu-item.scss";
+const MenuItem = ({ title, imageUrl, size }) => {
+  return (
+    <div
+      className={`${size} menu-item`}
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    >
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+export default MenuItem;
