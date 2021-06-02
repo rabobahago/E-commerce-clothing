328eff01a2cde5873768b02b50d19e1b1b5228cb
rabobahago <rabobahago@gmail.com>
2021-06-02 18:27:35 +0100

    menu-item scss added

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,12 +1,13 @@
 import "./menu-item.scss";
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div
-      className={`${size} menu-item`}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
