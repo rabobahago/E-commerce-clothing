55415f31b363ad9ee82129a92b8de11d43e9333e
rabobahago <rabobahago@gmail.com>
2021-06-05 13:14:49 +0100

    header component added for easy navigation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.jsx";
 import ShopPage from "./pages/shop/shop.jsx";
+import Header from "./components/header/header.jsx";
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
