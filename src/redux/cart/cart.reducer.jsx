6710aec994b42d2936920a04ea6923618b955cfd
rabobahago <rabobahago@gmail.com>
2021-06-10 22:09:59 +0100

    cart utils function added to to calculate quantity of items in the cart

--- src/redux/cart/cart.reducer.jsx
+++ src/redux/cart/cart.reducer.jsx
@@ -1,5 +1,6 @@
 import { CartActionTypes } from "./cart.types";
 import { CartIteMTypes } from "./cart.types";
+import { addItemToCart } from "./cart.utils";
 const INITIAL_STATE = {
   hidden: true,
   cartItem: [],
@@ -15,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartIteMTypes.ADD_ITEM:
       return {
         ...state,
-        cartItem: [...state.cartItem, action.payload],
+        cartItem: addItemToCart(state.cartItem, action.payload),
       };
     default:
       return state;
