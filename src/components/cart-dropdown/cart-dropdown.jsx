ce19b00087630230ba1bfcf6da5f485fd042b746
rabobahago <rabobahago@gmail.com>
2021-06-12 18:13:25 +0100

    toggle cart hidden added check out button

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { createStructuredSelector } from "reselect";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { selectICartItems } from "../../redux/cart/cart.selectors";
 import CustomButton from "../custom-button/custom-button";
 import CartItem from "../cart-item/cart-item";
 import "./cart-dropdown.scss";
 
-const CartDropdown = ({ cartItem, history }) => (
+const CartDropdown = ({ cartItem, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItem.length ? (
@@ -18,7 +19,12 @@ const CartDropdown = ({ cartItem, history }) => (
         <span className="empty-message">Your cart is empty</span>
       )}
     </div>
-    <CustomButton onClick={() => history.push("/checkout")}>
+    <CustomButton
+      onClick={() => {
+        history.push("/checkout");
+        dispatch(toggleCartHidden());
+      }}
+    >
       CHECKOUT
     </CustomButton>
   </div>
