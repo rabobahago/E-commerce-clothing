35d00e8cb366096e9c6f97587191e1315be61dbb
rabobahago <rabobahago@gmail.com>
2021-06-14 07:28:13 +0100

    redux persist added in other store data and not to loss data during refresh, moving to another tag, or browser closed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 const middleware = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middleware));
-
-export default store;
+export const store = createStore(rootReducer, applyMiddleware(...middleware));
+export const persistor = persistStore(store);
+//export default { store, persistor };
